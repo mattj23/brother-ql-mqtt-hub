a3584d8a0a25a5f7253e31f0e76e58acf024a406
Matt Jarvis <mattj23@gmail.com>
2021-08-27 11:22:03 -0400

    mqtt connection, data model started

---
+++ BrotherQlMqttHub/BrotherQlMqttHub/Data/HostMessage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BrotherQlMqttHub.Data
+{
+    public class HostMessage
+    {
+        public bool Online { get; set; }
+        public string Ip { get; set; }
+        public string Host { get; set; }
+        public int UpdateS { get; set; }
+
+        public List<PrinterInfo> Printers { get; set; }
+    }
+}
--- BrotherQlMqttHub/BrotherQlMqttHub/Services/PrinterMonitor.cs
+++ BrotherQlMqttHub/BrotherQlMqttHub/Services/PrinterMonitor.cs
@@ -1,45 +1,77 @@
+using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Security.Authentication;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using BrotherQlMqttHub.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace BrotherQlMqttHub.Services
 {
     public class PrinterMonitor : BackgroundService
     {
-        private IMqttClient _client;
+        private IManagedMqttClient _client;
         private readonly IConfigurationSection _configSection;
+        private readonly JsonSerializerSettings _jsonSettings;
+
 
         public PrinterMonitor(IConfigurationSection configSection)
         {
             _configSection = configSection;
+            _jsonSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                }
+            };
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var options = new MqttClientOptionsBuilder()
-                .WithTcpServer(_configSection["Host"], int.Parse(_configSection["Port"]))
-                .WithClientId("brother_ql_hub")
+            var options = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(new MqttClientOptionsBuilder()
+                    .WithTcpServer(_configSection["Host"], int.Parse(_configSection["Port"]))
+                    .WithTls(new MqttClientOptionsBuilderTlsParameters 
+                        {
+                            UseTls = true,
+                            SslProtocol = SslProtocols.Tls12
+                        })
+                    .WithClientId("brother_ql_hub")
+                    .Build())
                 .Build();
 
-            _client = new MqttFactory().CreateMqttClient();
+            _client = new MqttFactory().CreateManagedMqttClient();
             _client.UseApplicationMessageReceivedHandler(MessageHandler);
             _client.UseConnectedHandler(async e =>
             {
+                Debug.WriteLine("Connection to MQTT broker established");
                 // The connection occurs here
                 await _client.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("label_servers/status/#").Build());
             });
 
-            await _client.ConnectAsync(options, stoppingToken);
+            Debug.WriteLine("Attempting connection to MQTT broker...");
+            await _client.StartAsync(options);
         }
 
         private Task MessageHandler(MqttApplicationMessageReceivedEventArgs e)
         {
             Debug.WriteLine(e.ApplicationMessage.Topic);
+
+            var stringContents = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+
+            var message = JsonConvert.DeserializeObject<HostMessage>(stringContents, _jsonSettings);
+
 
             return Task.CompletedTask;
         }
